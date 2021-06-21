043be11610f8d8ab339c4c8fe48f12d9c8056247
pranavrajvanshi <64026650+pranavrajvanshi@users.noreply.github.com>
2021-06-21 20:19:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,80 @@
 //jshint esversion:6
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+
+
+const clubs = require('./routes/clubs');
+const reviews = require('./routes/reviews');
+
+mongoose.connect('mongodb://localhost:27017/clubs', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.use("/public", express.static("public"));
 
-//home page
-app.get("/", (req, res) => {
-  res.render("admin/club/index");
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.use('/clubs', clubs)
+app.use('/clubs/:id/reviews', reviews)
+
+app.get('/', (req, res) => {
+    res.render('home')
 });
 
-app.listen(3000, function () {
-  console.log("server running successfully on port 3000");
-});
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
+
+
